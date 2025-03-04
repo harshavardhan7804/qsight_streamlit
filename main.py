02e8906e2db422e08bc1307967888005a8bccfb3
Harsha Vardhan <harsha.vardhan@techolution.com>
2025-03-04 14:00:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import json
 import os
-from dotenv import load_dotenv
 import re
 import pandas as pd
 from scripts.vector_storage import VertexAIEmbeddingFunction, ChromaDBHandler
